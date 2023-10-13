b449632cde598690fe26c825778c36accbd46b24
Saravia1100 <diego.saravia@clearviewlive.com>
2023-10-13 11:39:57 -0600

    feat: routes for login actions and more pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,10 +17,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
 Route::prefix('catalogo')->group(function () {
     Route::get('/', function () {
         return view('catalogo.index');
     });
+});
+
+Route::get('login', function () {
+    return view('login');
+})->name('login_page');
+
+Route::prefix('users')->group(function () {
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+    Route::post('/register', [UserController::class, 'register'])->name('register');
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+});
+
+Route::middleware(Authenticate::class)->group(function () {
+    Route::get('/perfil', [UserController::class, 'profile'])->name('profile');
 });
