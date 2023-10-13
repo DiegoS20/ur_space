c61a86d88905b750a90805aa54b3488c95e81d47
Saravia1100 <diego.saravia@clearviewlive.com>
2023-10-13 01:05:45 -0600

    feat: base template for pages

---
+++ resources/views/templates/base.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>UR SPace</title>
+  {{-- Google fonts --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet" />
+
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@700&display=swap" rel="stylesheet" />
+
+  {{-- Bootstrap --}}
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+
+  <!-- FontAwesome 6.1.1 CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+  crossorigin="anonymous"
+    referrerpolicy="no-referrer" />
+
+  {{-- My styles or stylesheets --}}
+  <link rel="stylesheet" href="{{ mix('css/global.css') }}">
+  @yield('styles')
+</head>
+
+<body>
+  <x-navbar :black="isset($black) ? $black : false" />
+
+  @yield('content')
+
+  <x-footer />
+
+  <script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+  @yield('scripts')
+</body>
+
+</html>
