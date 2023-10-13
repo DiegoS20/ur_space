a9d6de12eebfefbdb2ee5b03860947c8063f7c3a
Saravia1100 <diego.saravia@clearviewlive.com>
2023-10-13 01:04:39 -0600

    feat: footer component only view

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,104 @@
+<footer class="text-center text-lg-start text-white" style="background-color: #000">
+  <!-- Grid container -->
+  <div class="container p-4 pb-0">
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Ur Space</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Architecto error consequuntur dolor vero similique quia
+            consectetur aperiam, mollitia nihil fugiat!
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none" />
+
+        <!-- Grid column -->
+        <div class="col-md-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Servicios</h6>
+          <p>
+            <a class="text-white linksnone" href="catalogo.html" target="_blank">Explora</a>
+          </p>
+          <p>
+            <a class="text-white linksnone" href="catalogo.html" target="_blank">Arrienda</a>
+          </p>
+          <p>
+            <a class="text-white linksnone" href="reservaform.html" target="_blank">Hospédate</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none" />
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">SOPORTE</h6>
+          <p>
+            <a class="text-white linksnone" href="soporte.html">Soporte</a>
+          </p>
+          <p>
+            <a class="text-white linksnone" href="soporte.html">Ayuda</a>
+          </p>
+        </div>
+
+        <!-- Grid column -->
+        <hr class="w-100 clearfix d-md-none" />
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contacto</h6>
+          <p><i class="fas fa-envelope mr-3"></i> UrSpace@info.com</p>
+          <p><i class="fas fa-phone mr-3"></i> + 503 0000-0000</p>
+          <p><i class="fas fa-print mr-3"></i> + 503 0000-0000</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+
+    <hr class="my-3" />
+
+    <!-- Section: Copyright -->
+    <section class="p-3 pt-0">
+      <div class="row d-flex align-items-center">
+        <!-- Grid column -->
+        <div class="col-md-7 col-lg-8 text-center text-md-start">
+          <!-- Copyright -->
+          <div class="p-3">
+            © 2023 Copyright:
+            <a class="text-white" target="_blank" href="https://www.google.com">Ur Space</a>
+          </div>
+          <!-- Copyright -->
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+          <!-- Facebook -->
+          <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i
+              class="fab fa-facebook-f"></i></a>
+
+          <!-- Twitter -->
+          <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i
+              class="fab fa-twitter"></i></a>
+
+          <!-- Google -->
+          <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i
+              class="fab fa-google"></i></a>
+
+          <!-- Instagram -->
+          <a class="btn btn-outline-light btn-floating m-1" class="text-white" role="button"><i
+              class="fab fa-instagram"></i></a>
+        </div>
+        <!-- Grid column -->
+      </div>
+    </section>
+    <!-- Section: Copyright -->
+  </div>
+</footer>
